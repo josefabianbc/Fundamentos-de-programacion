6cf0ff420e2cef1d6aa342155eda1dda1f03fcc0
josefabianbc <josefabianbonillacabrera@hotmail.com>
2023-10-12 22:28:14 -0400

    subiendo los ejercicios del cap 4

---
+++ programs cap 4/program4.3/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* Conflicto de variables con el mismo nombre. */
+void f1(void); /* Prototipo de función. */
+int K = 5; /* Variable global. */
+void main (void)
+{
+int I;
+for (I = 1; I <= 3; I++)
+f1();
+}
+void f1(void)
+{
+    int K = 2; /* Variable local. */
+    K += K;
+    printf("\n\nEl valor de la variable local es: %d", K);
+    ::K = ::K + K; /* Uso de ambas variables. */
+    printf("\nEl valor de la variable global es: %d", ::K);
+}
